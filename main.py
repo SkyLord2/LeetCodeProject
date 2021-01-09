cb8bcc51d3ad22746faefe362698f934df50efd3
SkyLord2 <chengdongsheng@outlook.com>
2021-01-09 10:40:06 +0800

    1011. 在 D 天内送达包裹的能力

--- main.py
+++ main.py
@@ -2342,8 +2342,42 @@ class Solution:
             res[i] = (stack[-1] - i) if(stack) else 0
             stack.append(i)
         return res
+    """
+    1011. 在 D 天内送达包裹的能力
+    传送带上的包裹必须在 D 天内从一个港口运送到另一个港口。
+    传送带上的第 i 个包裹的重量为 weights[i]。
+    每一天，我们都会按给出重量的顺序往传送带上装载包裹。我们装载的重量不会超过船的最大运载重量。
+    返回能在 D 天内将传送带上的所有包裹送达的船的最低运载能力。
+    """
+    def canFinish(self, weights: List[int], D: int, cap: int):
+        n = len(weights)
+        i = 0
+        for d in range(D):
+            _cap = cap
+            while((_cap := _cap - weights[i]) >= 0):
+                i += 1
+                if(i >= n):
+                    return True
+        return False
 
+    def shipWithinDays(self, weights: List[int], D: int) -> int:
+        """
+        运载能力的范围[max(weights), sum(weights)], 1 =< D <= len(weights)
+        :param weights:
+        :param D:
+        :return:
+        """
+        left = max(weights)
+        right = sum(weights) + 1
 
+        while(left < right):
+            mid = left + (right - left)//2
+            # 能完成
+            if(self.canFinish(weights, D, mid)):
+                right = mid
+            else:
+                left = mid + 1
+        return left
 
 
 
