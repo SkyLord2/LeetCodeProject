d4509e4fc8063b1a9a2ab235096ec7130826cd8a
SkyLord2 <chengdongsheng@outlook.com>
2021-01-14 21:21:55 +0800

    583. 两个字符串的删除操作 1143. 最长公共子序列

--- main.py
+++ main.py
@@ -2518,8 +2518,42 @@ class Solution:
                     dp[i][j] = dp[i-1][j]
         return dp[n][target]
 
+    """
+    1143. 最长公共子序列
+    给定两个字符串 text1 和 text2，返回这两个字符串的最长公共子序列的长度。
+    一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+    例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。两个字符串的「公共子序列」是这两个字符串所共同拥有的子序列。
+    若这两个字符串没有公共子序列，则返回 0。
+    """
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n1 = len(text1)
+        n2 = len(text2)
 
+        if(n1 == 0 or n2 == 0):
+            return 0
 
+        # dp数组，dp[i][j]表示 text1[...i]与text2[...j]之间的最长公共字串的长度
+        # 边界条件 dp[0][j] = dp[i][0] = 0
+        dp = [[0 for _ in range(n2+1)] for _ in range(n1+1)]
+
+        for i in range(1, n1+1):
+            for j in range(1, n2+1):
+                # 字符相同，说明 text1[i-1] 和 text2[j-1] 都在子序列中
+                if(text1[i-1] == text2[j-1]):
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        return dp[n1][n2]
+    """
+    583. 两个字符串的删除操作
+    给定两个单词 word1 和 word2，找到使得 word1 和 word2 相同所需的最小步数，每步可以删除任意一个字符串中的一个字符。
+    """
+    def minDistance(self, word1: str, word2: str) -> int:
+        n1 = len(word1)
+        n2 = len(word2)
+        n = self.longestCommonSubsequence(word1, word2)
+        res = n1 + n2 - 2 * n
+        return res
 
 
 # Press the green button in the gutter to run the script.
