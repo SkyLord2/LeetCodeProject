1805c1d1a9ed91ee31dc60614d1a8335307543b0
SkyLord2 <chengdongsheng@outlook.com>
2021-01-27 21:50:58 +0800

    969. 煎饼排序

--- main.py
+++ main.py
@@ -2953,7 +2953,38 @@ class Solution:
             res.append(int(input))
         self.memo[input] = res
         return res
+    """
+    969. 煎饼排序
+    给你一个整数数组 arr ，请使用 煎饼翻转 完成对数组的排序。
 
+    一次煎饼翻转的执行过程如下：
+    选择一个整数 k ，1 <= k <= arr.length
+    反转子数组 arr[0...k-1]（下标从 0 开始）
+    例如，arr = [3,2,1,4] ，选择 k = 3 进行一次煎饼翻转，反转子数组 [3,2,1] ，得到 arr = [1,2,3,4] 。  
+    以数组形式返回能使 arr 有序的煎饼翻转操作所对应的 k 值序列。任何将数组排序且翻转次数在 10 * arr.length 范围内的有效答案都将被判断为正确。  
+    """
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        res = []
+        def reverse(arr: List[int], start: int, end: int):
+            while(start < end):
+                arr[start], arr[end] = arr[end], arr[start]
+                start += 1
+                end -= 1
+
+        def sort(arr: List[int], n: int):
+            if(n == 1):
+                return
+            # 最大值的索引
+            arrPart = arr[:n]
+            maxIdx = arrPart.index(max(arrPart))
+            reverse(arr, 0, maxIdx)
+            res.append(maxIdx + 1)
+            reverse(arr, 0, n-1)
+            res.append(n)
+            sort(arr, n-1)
+
+        sort(arr, len(arr))
+        return res
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
