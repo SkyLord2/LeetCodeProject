6a4de0f064c172822cb0f8ab26c82c7890d5d0a6
SkyLord2 <chengdongsheng@outlook.com>
2021-01-31 16:17:15 +0800

    322. 零钱兑换

--- session02.py
+++ session02.py
@@ -6,6 +6,18 @@
 # @File : session02.py
 # @Software: PyCharm
 
+import sys
+import traceback
+from typing import List
+from queue import Queue
+from queue import PriorityQueue
+from collections import defaultdict
+import heapq
+import cmath
+import random
+import functools
+import bisect
+
 class Solution:
     def __init__(self):
         pass
@@ -31,3 +43,24 @@ class Solution:
             dp[i] = dp[i - 1] + dp[i - 2]
 
         return dp[n]
+    """
+    322. 零钱兑换
+    给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。
+    如果没有任何一种硬币组合能组成总金额，返回 -1。
+    你可以认为每种硬币的数量是无限的。
+    """
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # dp[i]表示凑成金额 i 所需要的最少的硬币个数
+        dp = [float('inf') for _ in range(amount + 1)]
+        # 边界条件
+        dp[0] = 0
+
+        for i in range(1, amount + 1):
+            # 状态转移
+            if(i in coins):
+                dp[i] = 1
+            else:
+                for c in coins:
+                    if((i-c) >= 0):
+                        dp[i] = min(dp[i-c] + 1, dp[i])
+        return dp[amount] if(dp[amount] != float('inf')) else -1
