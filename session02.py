cb740cbb45b9d6aa981ba27f08640c2a940155e1
SkyLord2 <chengdongsheng@outlook.com>
2021-02-06 20:03:07 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置

--- session02.py
+++ session02.py
@@ -287,3 +287,42 @@ class Solution:
             else:
                 left = middle + 1
         return -1
+    """
+    34. 在排序数组中查找元素的第一个和最后一个位置
+    给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+    如果数组中不存在目标值 target，返回 [-1, -1]。
+
+    进阶：
+    你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+    """
+    def searchLeftBound(self, nums: List[int], target):
+        n = len(nums)
+        if (n == 0):
+            return -1
+        left = 0
+        right = n
+
+        while (left < right):
+            middle = left + (right - left) // 2
+            it = nums[middle]
+            if (it == target):
+                right = middle
+            elif (it > target):
+                right = middle
+            else:
+                left = middle + 1
+        if (left >= n):
+            return -1
+        return -1 if (nums[left] != target) else left
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        idx = self.searchLeftBound(nums, target)
+
+        if(idx == -1):
+            return [-1, -1]
+        else:
+            right = idx
+            for i in range(idx+1, n):
+                if(nums[i] == target):
+                    right = i
+            return [idx, right]
