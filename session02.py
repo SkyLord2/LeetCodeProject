e194ad8584049c7a1d5ebdac580df7b486538ff2
SkyLord2 <chengdongsheng@outlook.com>
2021-02-13 10:18:43 +0800

    121. 买卖股票的最佳时机,

--- session02.py
+++ session02.py
@@ -536,3 +536,23 @@ class Solution:
             # 当前持有股票，昨天就持有，或者昨天未持有但是今天第一次买入 0 - prices[i]
             dp[i][1] = max(dp[i-1][1], - prices[i])
         return dp[n-1][0]
+    """
+    122. 买卖股票的最佳时机 II
+    给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+    设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+    注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+    """
+    def maxProfitII(self, prices: List[int]) -> int:
+        n = len(prices)
+        if(n == 0):
+            return 0
+        dp = [[0,0 ] for _ in range(n)]
+        dp[0][0] = 0
+        dp[0][1] = -prices[0]
+        # 只能进行一次买入卖出
+        for i in range(1, n):
+            # 当前未持有股票，昨天未持有，或者昨天持有但是今天卖出
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+            # 当前持有股票，昨天就持有，或者昨天未持有但是今天第一次买入 0 - prices[i]
+            dp[i][1] = max(dp[i - 1][1], dp[i-1][0] - prices[i])
+        return dp[n - 1][0]
