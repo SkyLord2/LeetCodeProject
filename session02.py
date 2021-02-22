40e4c7488f533952f72907199ea0cfdfeb34bd56
SkyLord2 <chengdongsheng@outlook.com>
2021-02-22 20:12:02 +0800

    213. 打家劫舍 II

--- session02.py
+++ session02.py
@@ -680,3 +680,36 @@ class Solution:
             # 两者之间的最大值
             dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
         return dp[n]
+
+    """
+    213. 打家劫舍 II
+    你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈 ，
+    这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，
+    系统会自动报警 。
+
+    给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，能够偷窃到的最高金额。
+    """
+    def robII(self, nums: List[int]) -> int:
+        def robRange(nums):
+            n = len(nums)
+            if (n == 0):
+                return 0
+            # dp 表示前 n 间住房所能偷盗的最大金额
+            dp = [0 for _ in range(n + 1)]
+            # 边界条件，没有住房可偷为 0，只有一间住房时，只能偷这个房间的金额 nums[0]
+            dp[0] = 0
+            dp[1] = nums[0]
+            for i in range(2, n + 1):
+                # dp[i] 前 i 间住房所能偷盗的最大金额可以表示为：
+                # 前 i-1 间住房所能偷盗的最大金额
+                # 前 i-2 间住房所能偷盗的最大金额 + 第 i 间住房的金额
+                # 两者之间的最大值
+                dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1])
+            return dp[n]
+        n = len(nums)
+        if(n == 1):
+            return nums[0]
+        # 最后一间住房与第一件住房不能同时被抢：
+        # 抢最后一间住房 或者 抢第一间住房
+        res = max(robRange(nums[0:n-1]), robRange(nums[1:]))
+        return res
