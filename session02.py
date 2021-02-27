cba2e7e7e98986f198e09b11559f9b8450662bdd
SkyLord2 <chengdongsheng@outlook.com>
2021-02-27 16:10:27 +0800

    986. 区间列表的交集

--- session02.py
+++ session02.py
@@ -802,3 +802,32 @@ class Solution:
                 end = e
                 ret.append([start, end])
         return ret
+    """
+    986. 区间列表的交集
+    给定两个由一些 闭区间 组成的列表，firstList 和 secondList ，其中 firstList[i] = [starti, endi] 
+    而 secondList[j] = [startj, endj] 。每个区间列表都是成对 不相交 的，并且 已经排序 。
+    返回这 两个区间列表的交集 。
+    形式上，闭区间 [a, b]（其中 a <= b）表示实数 x 的集合，而 a <= x <= b 。
+    两个闭区间的 交集 是一组实数，要么为空集，要么为闭区间。例如，[1, 3] 和 [2, 4] 的交集为 [2, 3]。
+    """
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        n = len(firstList)
+        m = len(secondList)
+        if(not n or not m):
+            return []
+
+        res = []
+        i, j = 0, 0
+        while(i < n and j < m):
+            a1, a2 = firstList[i][0], firstList[i][1]
+            b1, b2 = secondList[j][0], secondList[j][1]
+            # 有交集的情况, !(a2 < b1 or b2 < a1)
+            if(b1 <= a2 and a1 <= b2):
+                c1 = max(a1, b1)
+                c2 = min(a2, b2)
+                res.append([c1, c2])
+            if(b2 < a2):
+                j += 1
+            else:
+                i += 1
+        return res
