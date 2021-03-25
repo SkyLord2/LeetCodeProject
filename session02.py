47e5300ce873723880b34350b93679bf856a958d
SkyLord2 <chengdongsheng@outlook.com>
2021-03-25 20:07:31 +0800

    230. 二叉搜索树中第K小的元素

--- session02.py
+++ session02.py
@@ -1271,3 +1271,19 @@ class Solution:
             return rootStr
         traverse(root)
         return res
+    """
+    230. 二叉搜索树中第K小的元素
+    给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。
+    """
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        res = []
+        def traverse(root: TreeNode):
+            if(not root):
+                return
+            traverse(root.left)
+            if(len(res) == k):
+                return
+            res.append(root.val)
+            traverse(root.right)
+        traverse(root)
+        return res[-1]
