ca337930ac651626283c095aad02e27224383241
SkyLord2 <chengdongsheng@outlook.com>
2021-03-27 22:30:18 +0800

    450. 删除二叉搜索树中的节点

--- session02.py
+++ session02.py
@@ -1312,3 +1312,37 @@ class Solution:
 
         traverseSum(root)
         return root
+    """
+    450. 删除二叉搜索树中的节点
+    给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key 对应的节点，并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的根节点的引用。
+
+    一般来说，删除节点可分为两个步骤：
+    
+    首先找到需要删除的节点；
+    如果找到了，删除它。
+    说明： 要求算法时间复杂度为 O(h)，h 为树的高度。
+    """
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        def getMin(root: TreeNode):
+            if(not root):
+                return None
+            while(root.left):
+                root = root.left
+            return root
+        if(not root):
+            return None
+        if(root.val == key):
+            if(not root.left and not root.right):
+                return None
+            elif(not root.right):
+                return root.left
+            elif(not root.left):
+                return root.right
+            minNode = getMin(root.right)
+            root.val = minNode.val
+            root.right = self.deleteNode(root.right, minNode.val)
+        elif(root.val > key):
+            root.left = self.deleteNode(root.left, key)
+        elif(root.val < key):
+            root.right = self.deleteNode(root.right, key)
+        return root
