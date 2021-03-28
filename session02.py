d3686093dbab5729282dc3652d28330967da9a14
SkyLord2 <chengdongsheng@outlook.com>
2021-03-28 10:22:55 +0800

    98. 验证二叉搜索树

--- session02.py
+++ session02.py
@@ -1373,3 +1373,23 @@ class Solution:
             return self.searchBST(root.right, val)
         elif (root.val > val):
             return self.searchBST(root.left, val)
+    """
+    98. 验证二叉搜索树
+    给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+
+    假设一个二叉搜索树具有如下特征：
+    
+    节点的左子树只包含小于当前节点的数。
+    节点的右子树只包含大于当前节点的数。
+    所有左子树和右子树自身必须也是二叉搜索树。
+    """
+    def isValidBST(self, root: TreeNode) -> bool:
+        def isValid(root: TreeNode, min: TreeNode, max: TreeNode):
+            if(not root):
+                return True
+            elif(max and root.val <= max.val):
+                return False
+            elif(min and root.val >= min.val):
+                return False
+            return isValid(root.left, root, max) and isValid(root.right, min, root)
+        return isValid(root, None, None)
