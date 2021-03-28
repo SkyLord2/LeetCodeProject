21aaa387d58fd6eccbc856a0e07df10bb69dbaa9
SkyLord2 <chengdongsheng@outlook.com>
2021-03-28 09:44:17 +0800

    700. 二叉搜索树中的搜索

--- session02.py
+++ session02.py
@@ -1360,3 +1360,16 @@ class Solution:
         elif (root.val > val):
             root.left = self.insertIntoBST(root.left, val)
         return root
+    """
+    700. 二叉搜索树中的搜索
+    给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+    """
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if (not root):
+            return None
+        elif (root.val == val):
+            return root
+        elif (root.val < val):
+            return self.searchBST(root.right, val)
+        elif (root.val > val):
+            return self.searchBST(root.left, val)
