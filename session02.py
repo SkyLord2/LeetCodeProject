1f478729ff2f177d30384305e9ac55fe00352935
SkyLord2 <chengdongsheng@outlook.com>
2021-04-13 20:40:46 +0800

    990. 等式方程的可满足性

--- session02.py
+++ session02.py
@@ -1651,3 +1651,24 @@ class Solution:
                 if(board[i][j] == 'O'):
                     if(not uf.connected(dummy, i * n + j)):
                         board[i][j] = 'X'
+    """
+    990. 等式方程的可满足性
+    给定一个由表示变量之间关系的字符串方程组成的数组，每个字符串方程 equations[i] 的长度为 4，
+    并采用两种不同的形式之一："a==b" 或 "a!=b"。在这里，a 和 b 是小写字母（不一定不同），表示单字母变量名。
+    只有当可以将整数分配给变量名，以便满足所有给定的方程时才返回 true，否则返回 false。
+    """
+    def equationsPossible(self, equations: List[str]) -> bool:
+        if(not equations):
+            return
+        # == 关系表示连通性
+        a = ord('a')
+        uf = UnionFind(26)
+        for equation in equations:
+            if(equation[1] == '='):
+                uf.union(ord(equation[0])-a, ord(equation[3])-a)
+        # != 关系表示不连通
+        for equation in equations:
+            if(equation[1] == '!'):
+                if(uf.connected(ord(equation[0])-a, ord(equation[3])-a)):
+                    return False
+        return True
