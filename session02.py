14db23b7a047011cae8c144ac97ce9f2679c2f7a
SkyLord2 <chengdongsheng@outlook.com>
2021-06-14 11:00:12 +0800

    355. 设计推特

--- session02.py
+++ session02.py
@@ -522,6 +522,125 @@ class MedianFinder:
             return self.maxHeap.getMax()
         else:
             return (self.minHeap.getMin() + self.maxHeap.getMax())/2
+
+"""
+355. 设计推特
+设计一个简化版的推特(Twitter)，可以让用户实现发送推文，关注/取消关注其他用户，能够看见关注人（包括自己）的最近十条推文。你的设计需要支持以下的几个功能：
+
+postTweet(userId, tweetId): 创建一条新的推文
+getNewsFeed(userId): 检索最近的十条推文。每个推文都必须是由此用户关注的人或者是用户自己发出的。推文必须按照时间顺序由最近的开始排序。
+follow(followerId, followeeId): 关注一个用户
+unfollow(followerId, followeeId): 取消关注一个用户
+"""
+class Twitter:
+    timestamp = 0
+    class Tweet:
+        def __init__(self, id, time):
+            # tweet的id
+            self.id = id
+            # tweet的发布时间
+            self.time = time
+            # 下一条推特
+            self.next = None
+        def __lt__(self, other):
+            """
+            降序排列
+            :param other:
+            :return:
+            """
+            return self.time > other.time
+
+    class User:
+        def __init__(self, userId):
+            # 用户的Id
+            self.userId = userId
+            # 用户的关注列表
+            self.followed = set()
+            # 用户发布的tweet
+            self.head = None
+            # 用户关注自己
+            self.follow(userId)
+
+        def follow(self, userId):
+            self.followed.add(userId)
+
+        def unfollow(self, userId):
+            # 不能取关自己
+            if(self.userId != userId):
+                if(userId in self.followed):
+                    self.followed.remove(userId)
+        def post(self, id):
+            tweet = Twitter.Tweet(id, Twitter.timestamp)
+            Twitter.timestamp += 1
+            # 越新的tweet越靠前
+            tweet.next = self.head
+            # 最新的tweet作为头节点
+            self.head = tweet
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.userMap = {}
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        """
+        Compose a new tweet.
+        """
+        if(userId not in self.userMap):
+            user = Twitter.User(userId)
+            self.userMap[userId] = user
+        user = self.userMap[userId]
+        user.post(tweetId)
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        """
+        Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+        """
+        if(userId not in self.userMap):
+            return []
+        pq = PriorityQueue()
+        ret = []
+        user = self.userMap[userId]
+        for followedId in user.followed:
+            followee = self.userMap[followedId]
+            if(followee.head):
+                pq.put(followee.head)
+
+        while(not pq.empty()):
+            if(len(ret) == 10):
+                break
+            tweet = pq.get()
+            ret.append(tweet.id)
+            if(tweet.next):
+                pq.put(tweet.next)
+
+        return ret
+
+
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        """
+        Follower follows a followee. If the operation is invalid, it should be a no-op.
+        """
+        if (followerId not in self.userMap):
+            follower = Twitter.User(followerId)
+            self.userMap[followerId] = follower
+        if (followeeId not in self.userMap):
+            followee = Twitter.User(followeeId)
+            self.userMap[followeeId] = followee
+        follower = self.userMap[followerId]
+        follower.follow(followeeId)
+
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        """
+        Follower unfollows a followee. If the operation is invalid, it should be a no-op.
+        """
+        if (followerId in self.userMap):
+            follower = self.userMap[followerId]
+            follower.unfollow(followeeId)
+
 class Solution:
     def __init__(self):
         self.memo = {}
