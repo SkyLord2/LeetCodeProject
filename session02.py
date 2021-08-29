7fb34bae41a215ef7a395a48eecd19232fea72a3
SkyLord2 <chengdongsheng@outlook.com>
2021-08-29 10:17:59 +0800

    232. 用栈实现队列

--- session02.py
+++ session02.py
@@ -642,6 +642,68 @@ class Twitter:
             follower = self.userMap[followerId]
             follower.unfollow(followeeId)
 
+"""
+232. 用栈实现队列
+请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：实现 MyQueue 类：
+
+void push(int x) 将元素 x 推到队列的末尾
+int pop() 从队列的开头移除并返回元素
+int peek() 返回队列开头的元素
+boolean empty() 如果队列为空，返回 true ；否则，返回 false
+ 
+
+说明：
+你只能使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
+你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+"""
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.init = []
+        self.reverse = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.init.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        ret = None
+        self.carry()
+        if(self.reverse):
+            ret = self.reverse.pop()
+        return ret
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        self.carry()
+        ret = None
+        if(self.reverse):
+            ret = self.reverse[-1]
+        return ret
+
+    def carry(self):
+        if (not self.reverse):
+            while (self.init):
+                self.reverse.append(self.init.pop())
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return not self.init and not self.reverse
+
+
+
 class Solution:
     def __init__(self):
         self.memo = {}
