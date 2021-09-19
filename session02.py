7b7e0bb650a342a38bd72df38ed7828ad7fb7fbd
SkyLord2 <chengdongsheng@outlook.com>
2021-09-19 12:33:09 +0800

    875. 爱吃香蕉的珂珂

--- session02.py
+++ session02.py
@@ -2343,3 +2343,31 @@ class Solution:
                 res[ele.idx] = nums1[left]
                 left += 1
         return res
+    """
+    875. 爱吃香蕉的珂珂
+    珂珂喜欢吃香蕉。这里有 N 堆香蕉，第 i 堆中有 piles[i] 根香蕉。警卫已经离开了，将在 H 小时后回来。
+
+    珂珂可以决定她吃香蕉的速度 K （单位：根/小时）。每个小时，她将会选择一堆香蕉，从中吃掉 K 根。如果这堆香蕉少于 K 根，
+    她将吃掉这堆的所有香蕉，然后这一小时内不会再吃更多的香蕉。
+    珂珂喜欢慢慢吃，但仍然想在警卫回来前吃掉所有的香蕉。
+    返回她可以在 H 小时内吃掉所有香蕉的最小速度 K（K 为整数）。
+    """
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def rightSpeed(k, piles):
+            total = 0
+            for (idx, item) in enumerate(piles):
+                total += item // k
+                if(item % k > 0):
+                    total += 1
+            return total
+        maxSpeed = max(piles) + 1
+        minSpeed = 1
+        k = 0
+        while (minSpeed < maxSpeed):
+            k = minSpeed + (maxSpeed - minSpeed) // 2
+            total = rightSpeed(k, piles)
+            if(total <= h):
+                maxSpeed = k
+            else:
+                minSpeed = k + 1
+        return minSpeed
