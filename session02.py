5920157c4ba953f6272208dc0d04c576fe4a3362
SkyLord2 <chengdongsheng@outlook.com>
2021-09-20 10:41:36 +0800

    1011. 在 D 天内送达包裹的能力

--- session02.py
+++ session02.py
@@ -2371,3 +2371,35 @@ class Solution:
             else:
                 minSpeed = k + 1
         return minSpeed
+    """
+    1011. 在 D 天内送达包裹的能力
+    传送带上的包裹必须在 D 天内从一个港口运送到另一个港口。
+    传送带上的第 i 个包裹的重量为 weights[i]。每一天，我们都会按给出重量的顺序往传送带上装载包裹。我们装载的重量不会超过船的最大运载重量。
+    返回能在 D 天内将传送带上的所有包裹送达的船的最低运载能力。
+    """
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        def reachDays(load):
+            d = 0
+            l = len(weights)
+            i = 0
+            while i < l:
+                x = load
+                while (i < l):
+                    if (x < weights[i]):
+                        break
+                    else:
+                        x -= weights[i]
+                    i += 1
+                d += 1
+            return d
+
+        minLoad = max(weights)
+        maxLoad = sum(weights) + 1
+        while (minLoad < maxLoad):
+            load = minLoad + (maxLoad - minLoad) // 2
+            d = reachDays(load)
+            if (d <= days):
+                maxLoad = load
+            else:
+                minLoad = load + 1
+        return minLoad
