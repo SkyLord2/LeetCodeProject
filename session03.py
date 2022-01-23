a055772de096ac229db72083f49d8c8b6c916821
SkyLord2 <chengdongsheng@outlook.com>
2022-01-23 10:02:34 +0800

    72. 编辑距离

--- session03.py
+++ session03.py
@@ -122,3 +122,40 @@ class Solution:
                     c.append(dp[i-1][j+1] + cur)
                 dp[i][j] = min(c)
         return min(dp[n-1])
+    """
+    72. 编辑距离
+    给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数。
+    你可以对一个单词进行如下三种操作：
+    插入一个字符
+    删除一个字符
+    替换一个字符
+    
+    提示：
+    0 <= word1.length, word2.length <= 500
+    word1 和 word2 由小写英文字母组成
+    """
+    def minDistance(self, word1: str, word2: str) -> int:
+        n1 = len(word1)
+        n2 = len(word2)
+        if (n1 == 0):
+            return n2
+        if (n2 == 0):
+            return n1
+        # dp 数组 dp[i][j]表示 word1[0 ~ i-1]前i个字母 word2[0 ~ j-1]前j个字母之间的最小编辑距离
+        dp = [[0 for j in range(n2 + 1)] for i in range(n1 + 1)]
+        # base case
+        for i in range(1, n1 + 1):
+            dp[i][0] = i
+        for j in range(1, n2 + 1):
+            dp[0][j] = j
+        # 状态转移
+        for i in range(1, n1 + 1):
+            for j in range(1, n2 + 1):
+                if (word1[i - 1] == word2[j - 1]):
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    # dp[i - 1][j] + 1          插入操作
+                    # dp[i][j - 1] + 1          移除操作
+                    # dp[i - 1][j - 1] + 1      替换操作
+                    dp[i][j] = min([dp[i - 1][j - 1] + 1, dp[i - 1][j] + 1, dp[i][j - 1] + 1])
+        return dp[n1][n2]
